bfe537e60620aa59491a021045d1e1ccad22058f
aaroncustodio <custodio.aaron.1197@gmail.com>
2021-02-17 23:07:19 +0800

    rename datemodified

--- SRS_Generator/Infrastructure/BaseEntity.cs
+++ SRS_Generator/Infrastructure/BaseEntity.cs
@@ -6,6 +6,6 @@ namespace SRS_Generator.Infrastructure
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public DateTime DateCreated { get; set; } = DateTime.UtcNow;
-        public DateTime DateModified { get; set; } = DateTime.UtcNow;
+        public DateTime DateUpdated { get; set; } = DateTime.UtcNow;
     }
 }
