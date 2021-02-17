11944cac97da8f9c6ae95219bd7c8f1ede00f40d
aaroncustodio <custodio.aaron.1197@gmail.com>
2021-02-17 23:07:07 +0800

    update appsettings

--- SRS_Generator/Config/CommandSettings.cs
+++ SRS_Generator/Config/CommandSettings.cs
@@ -1,7 +1,10 @@
-﻿namespace SRS_Generator.Config
+﻿using System.Collections.Generic;
+
+namespace SRS_Generator.Config
 {
     public class CommandSettings
     {
         public string Prefix { get; set; }
+        public List<string> Prefixes { get; set; }
     }
 }
