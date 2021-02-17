4a3b614dc17bc195b826b8ddfc53a353762544f3
aaroncustodio <custodio.aaron.1197@gmail.com>
2021-02-17 23:04:27 +0800

    add commands to test database

--- SRS_Generator/Commands/FunCommands.cs
+++ SRS_Generator/Commands/FunCommands.cs
@@ -1,11 +1,21 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using SRS_Generator.Services;
+using SRS_Generator.ViewModels;
 using System.Threading.Tasks;
 
 namespace SRS_Generator.Commands
 {
     public class FunCommands : BaseCommandModule
     {
+        private readonly IGuildService _guildService;
+
+        public FunCommands(
+            IGuildService guildService)
+        {
+            _guildService = guildService;
+        }
+
         [Command("ping")]
         [Description("Returns pong")]
         public async Task Ping(CommandContext ctx)
@@ -24,10 +34,30 @@ namespace SRS_Generator.Commands
             await ctx.Channel.SendMessageAsync(message).ConfigureAwait(false);
         }
 
-        [Command("addguild")]
-        public async Task AddGuild(CommandContext ctx, string name)
+        [Command("user-info")]
+        [Description("Returns user information")]
+        public async Task GetUserInfo(CommandContext ctx)
         {
+            var member = ctx.Member;
+            var username = member.Username;
+            string message = $@"{username}
+                {member.Discriminator}
 
+                {member.DisplayName}";
+            await ctx.Channel.SendMessageAsync(message).ConfigureAwait(false);
+        }
+
+        [Command("add-guild")]
+        public async Task AddGuild(CommandContext ctx, string name, bool isActive, bool isFarming)
+        {
+            var guild = new GuildViewModel();
+            guild.Name = name;
+            guild.IsActive = isActive;
+            guild.IsFarmingGuild = isFarming;
+
+            await _guildService.CreateGuild(guild);
+
+            await ctx.Channel.SendMessageAsync("Guild added").ConfigureAwait(false);
         }
     }
 }
