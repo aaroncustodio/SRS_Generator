bd5c35ecf91bd233ccc418b2773ad56b55b61a07
aaroncustodio <custodio.aaron.1197@gmail.com>
2021-02-20 20:24:40 +0800

    add extension method class

---
+++ SRS_Generator/Helpers/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SRS_Generator.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static string ToBold(this string str)
+        {
+            str = $"**{str}**";
+            return str;
+        }
+    }
+}
